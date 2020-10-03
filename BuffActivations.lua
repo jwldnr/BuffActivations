0ddda16b18d8baeb5f1e9bca5de41e04ce1bd446
Jean Alexander Woldner <alexander@woldner.io>
2020-10-03 11:24:05 +0200

    refactor generic

--- BuffActivations.lua
+++ BuffActivations.lua
@@ -1,16 +1,21 @@
 local AddonName, Addon = ...
 
 -- locals and speed
-local _G = _G
-local ActionButton_ShowOverlayGlow = _G.ActionButton_ShowOverlayGlow
-local ActionButton_HideOverlayGlow = _G.ActionButton_HideOverlayGlow
-local GetActionInfo = _G.GetActionInfo
-local GetMacroSpell = _G.GetMacroSpell
-local IsSpellOverlayed = _G.IsSpellOverlayed    
+local select = select
 
-local OnlyShowInCombat = true
+local UnitBuff = UnitBuff
 
-local ACTION_BARS = {
+local ActionButton_ShowOverlayGlow = ActionButton_ShowOverlayGlow
+local ActionButton_HideOverlayGlow = ActionButton_HideOverlayGlow
+
+local GetActionInfo = GetActionInfo
+
+local GetSpellInfo = GetSpellInfo
+local GetMacroSpell = GetMacroSpell
+
+local UnitAffectingCombat = UnitAffectingCombat
+
+local ACTION_BUTTON_TEMPLATES = {
     "ActionButton",
     "MultiBarBottomLeftButton",
     "MultiBarBottomRightButton",
@@ -18,36 +23,32 @@ local ACTION_BARS = {
     "MultiBarRightButton"
 }
 
-local TRACKED_BUFFS = {
-    [6673] = "Battle Shout", -- rank 1
-    [5242] = "Battle Shout", -- rank 2
+local UNIT_TAG_PLAYER = "player"
+
+local ONLY_SHOW_IN_COMBAT = false
+
+local ABILITY_TYPE_SPELL = "spell"
+local ABILITY_TYPE_MACRO = "macro"
+
+local ABILITIES = {
+    -- warrior
+    ["Battle Shout"] = true,
+
+    -- priest
+    ["Power Word: Fortitude"] = true,
+
+    -- warlock
+    ["Soulstone"] = true
 }
 
-local function ShouldTrackBuff(id)
-    return TRACKED_BUFFS[id] ~= nil
-end
-
-
-function Addon:HasBuff(id)
+function Addon:HasBuff(name)
     for i = 1, #self.buffs do
-        if (self.buffs[i] == id) then
+        if (self.buffs[i] == name) then
             return true
         end
     end
 
     return false
-end
-
-function Addon:GetButtonsById(id)
-    local buttons = {}
-
-    for button, value in pairs(self.buttons) do
-        if (value == id) then
-            buttons[button] = value
-        end
-    end
-
-    return buttons
 end
 
 function Addon:OnEvent(event, ...)
@@ -61,30 +62,29 @@ end
 function Addon:UpdateActionButtons()
     self.buttons = {}
 
-    for _, bar in ipairs(ACTION_BARS) do
+    for _, template in ipairs(ACTION_BUTTON_TEMPLATES) do
         for i = 1, 12 do
-            local button = _G[bar..i]
+            local button = _G[template..i]
             local type, id = GetActionInfo(button.action)
+            local name = nil
 
-            if (id and type == "spell") then
-                if (ShouldTrackBuff(id)) then
-                    self.buttons[button] = id
-                end
+            if (id and type == ABILITY_TYPE_SPELL) then
+                name = GetSpellInfo(id)
             end
 
-            if (id and type == "macro") then
-                local spellId = GetMacroSpell(id)
+            if (id and type == ABILITY_TYPE_MACRO) then
+                name = GetSpellInfo(select(1, GetMacroSpell(id)))
+            end
 
-                if (ShouldTrackBuff(spellId)) then
-                    self.buttons[button] = spellId
-                end
+            if (name and ABILITIES[name]) then
+                self.buttons[button] = name
             end
         end
     end
 end
 
 function Addon:HideButtonOverlays()
-    for button, value in pairs(self.buttons) do
+    for button, _ in pairs(self.buttons) do
         if (button.overlay and button.overlay:IsVisible()) then
             button.overlay.animOut:Play()
 
@@ -93,31 +93,27 @@ function Addon:HideButtonOverlays()
     end
 end
 
-function Addon:UpdateButtonOverlays()
-    self.buffs = {}
-
-    local i = 1
-    local name, _, _, _, _, _, _, _, _, id = UnitBuff("player", i)
-
-    while (name) do
-        self.buffs[#self.buffs + 1] = id
-
-        i = i + 1
-        name, _, _, _, _, _, _, _, _, id = UnitBuff("player", i)
-    end
-
-    if (self.onlyShowInCombat and not UnitAffectingCombat("player")) then
+function Addon:ToggleButtonOverlays()
+    if (ONLY_SHOW_IN_COMBAT and not UnitAffectingCombat(PLAYER)) then
         return
     end
 
-    for button, value in pairs(self.buttons) do
-        if (self:HasBuff(value)) then
-            -- print("got buff, hide overlay")
+    self.buffs = {}
 
+    local i = 1
+    local buff = UnitBuff(UNIT_TAG_PLAYER, i)
+
+    while (buff) do
+        self.buffs[#self.buffs + 1] = buff
+
+        i = i + 1
+        buff = UnitBuff(UNIT_TAG_PLAYER, i)
+    end
+
+    for button, name in pairs(self.buttons) do
+        if (self:HasBuff(name)) then
             ActionButton_HideOverlayGlow(button)
         else
-            -- print("missing buff, show overlay")
-
             ActionButton_ShowOverlayGlow(button)
         end
     end
@@ -127,20 +123,13 @@ function Addon:ADDON_LOADED(name)
     if (AddonName == name) then
         self.frame:UnregisterEvent("ADDON_LOADED")
         
-        print("BuffActivations loaded")
+        print(AddonName, "loaded")
     end
-end
-
-function Addon:ACTIONBAR_SLOT_CHANGED(slot)
-    local type, id = GetActionInfo(slot)
-
-    self:UpdateActionButtons()
-    self:UpdateButtonOverlays()
 end
 
 function Addon:UNIT_AURA()
     self:UpdateActionButtons()
-    self:UpdateButtonOverlays()
+    self:ToggleButtonOverlays()
 end
 
 function Addon:PLAYER_REGEN_ENABLED()
@@ -149,13 +138,10 @@ end
 
 function Addon:PLAYER_ENTERING_WORLD()
     self:UpdateActionButtons()
-    self:UpdateButtonOverlays()
+    self:ToggleButtonOverlays()
 end
 
 function Addon:Load()
-    self.buttons = {}
-    self.onlyShowInCombat = true -- TODO
-
     self.frame = CreateFrame("Frame", nil)
     
     self.frame:SetScript("OnEvent", function(_, ...)
@@ -163,12 +149,10 @@ function Addon:Load()
     end)
 
     self.frame:RegisterEvent("ADDON_LOADED")
-
-    self.frame:RegisterEvent("ACTIONBAR_SLOT_CHANGED")
-    self.frame:RegisterUnitEvent("UNIT_AURA", "player")
-
-    self.frame:RegisterEvent("PLAYER_REGEN_ENABLED")
     self.frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+    self.frame:RegisterEvent("PLAYER_REGEN_ENABLED")
+
+    self.frame:RegisterUnitEvent("UNIT_AURA", UNIT_TAG_PLAYER)
 end
 
 Addon:Load()
